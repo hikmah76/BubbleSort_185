d99f1e42b7ed0ecf9e77386f927a251a23a66e59
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-20 19:05:36 +0700

    void display

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -26,3 +26,14 @@ void input() {			// Procedure untuk input
 		cin >> a[i];
 	}
 }
+
+void display() {	// Procedure untuk menampilkan hasil
+	cout << endl;									// Output baris kosong
+	cout << "==================================" << endl;	 // Output ke layar
+	cout << "Element Array yang telah tersusun" << endl;	// Output ke layar
+	cout << "==================================" << endl;	// Output ke layar
+	for (int j = 0; j < n; j++) {					// Looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;						// Output ke layar
+	}
+	cout << endl;									// Output baris kosong
+}
